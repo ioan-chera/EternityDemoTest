62003b9f45f0581d1ed984fdaea6a0b6c91c3379
Ioan Chera <ioan.chera@gmail.com>
2019-10-28 15:25:05 +0200

    Added Hell Revealed demos!

---
+++ hr/uglydemo.bat
@@ -0,0 +1,8 @@
+@echo off
+rem  ******************************************************
+rem  * This batch file was created by Mark David Bowman's * 
+rem  *    DOOMBAT program. DOOMBAT can be obtained on     *
+rem  *  FTP.CDROM.COM, America Online, and anywhere else  *
+rem  *            this program winds up at.               *
+rem  ******************************************************
+doom2 -timedemo arsevile -file hr.wad mm2mus.wad    
