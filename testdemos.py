bc8c9cb39cce5d17d18d0e2725d9a16859e4a608
Ioan Chera <ioan.chera@gmail.com>
2020-03-08 09:21:19 +0200

    Fixed to work with Python 3
    Tested with 3.8

--- testdemos.py
+++ testdemos.py
@@ -7,6 +7,12 @@ import sys
 import time
 from joblib import Parallel, delayed
 
+# set the base args (common to all games)
+ARGS_BASE = ['eternity-port/eternity', '-base', 'eternity-port/base', '-user', 'eternity-port/user',
+             '-nodraw', '-nosound']
+ARGS_DOOM = ['-iwad', 'DOOM.WAD']
+ARGS_DOOM2 = ['-iwad', 'DOOM2.WAD']
+
 ###################################################################
 ###################################################################
 
@@ -30,90 +36,88 @@ def natural_keys(text):
 ###################################################################
 
 
-# 0. Get data from .data
-with open('demo-list.data', 'r') as demo_list:
-    demo_list_lines = demo_list.readlines()
-scenarios = []
-for line in demo_list_lines:
-    scenario = line.split()
-    if len(scenario) < 2:
-        continue
-    scenarios.append(scenario)
-
-# now we have the items
-# set the base args (common to all games)
-args_base = ['eternity-port/eternity', '-base', 'eternity-port/base', '-user', 'eternity-port/user',
-             '-nodraw', '-nosound']
-args_doom = ['-iwad', 'DOOM.WAD']
-args_doom2 = ['-iwad', 'DOOM2.WAD']
-
-
-# delete old entries
-file_list = [os.path.join('temp-logs', f) for f in os.listdir("temp-logs") if f.endswith(".txt")]
-for f in file_list:
-    os.remove(f)
-
-# iterate the table entries
-index = 0
-commands_list = []
-for scenario in scenarios:
-    args = list(args_base)
-    if scenario[0] == '1':
-        args.extend(args_doom)
-    elif scenario[0] == '2':
-        args.extend(args_doom2)
-    else:
-        continue
-    if len(scenario) > 2:
-        args.extend(scenario[2:])
-    # now look in the folder with demos
-    files = sorted(os.listdir(scenario[1]))
-    for f in files:
-        if f.lower().endswith('.lmp'):
-            demo_path = os.path.join(scenario[1], f)
-            args_complete = list(args)
-            args_complete.extend(['-fastdemo', demo_path])
-            temp_log_path = os.path.join('temp-logs', str(index) + '.txt')
-            args_complete.extend(['-demolog', temp_log_path])
-            commands_list.append(args_complete)
-            index += 1
-
-# now we have the commands list. print it
-cpu_count = multiprocessing.cpu_count()
-
-print('Testing', len(commands_list), 'demos.')
-
-black_hole = open(os.devnull, 'w')
-
-
 def call_eternity(a):
-    subprocess.call(a, stdout=black_hole)
-    sys.stdout.write('.')
-    sys.stdout.flush()
-    
+    with open(os.devnull, 'w') as black_hole:
+        subprocess.call(a, stdout=black_hole)
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
-time_start = time.time()
 
-Parallel(n_jobs=cpu_count)(delayed(call_eternity)(a) for a in commands_list)
+def run_program():
+    # 0. Get data from .data
+    with open('demo-list.data', 'r') as demo_list:
+        demo_list_lines = demo_list.readlines()
+    scenarios = []
+    for line in demo_list_lines:
+        scenario = line.split()
+        if len(scenario) < 2:
+            continue
+        scenarios.append(scenario)
 
-time_end = time.time()
-time_elapsed = time_end - time_start
+    # now we have the items
 
-print()
-
-print('Elapsed time:', time_elapsed, '; average per demo: ', time_elapsed / len(commands_list))
-
-file_list = [os.path.join('temp-logs', f) for f in sorted(os.listdir("temp-logs"), key=natural_keys) if f.endswith(".txt")]
-
-try:
-    os.remove('demolog.txt')
-except OSError:
-    pass
-    
-with open('demolog.txt', 'w') as outfile:
-    for f in file_list:
-        with open(f) as infile:
-            outfile.write(infile.read())
+    # delete old entries
+    temp_file_list = [os.path.join('temp-logs', f) for f in os.listdir("temp-logs") if f.endswith(".txt")]
+    for f in temp_file_list:
         os.remove(f)
 
-subprocess.call(['osascript', '-e', 'display notification "Demo testing done!" with title "Eternity demo test"'])
+    # iterate the table entries
+    index = 0
+    commands_list = []
+    for scenario in scenarios:
+        args = list(ARGS_BASE)
+        if scenario[0] == '1':
+            args.extend(ARGS_DOOM)
+        elif scenario[0] == '2':
+            args.extend(ARGS_DOOM2)
+        else:
+            continue
+        if len(scenario) > 2:
+            args.extend(scenario[2:])
+        # now look in the folder with demos
+        files = sorted(os.listdir(scenario[1]))
+        for f in files:
+            if f.lower().endswith('.lmp'):
+                demo_path = os.path.join(scenario[1], f)
+                args_complete = list(args)
+                args_complete.extend(['-fastdemo', demo_path])
+                temp_log_path = os.path.join('temp-logs', str(index) + '.txt')
+                args_complete.extend(['-demolog', temp_log_path])
+                commands_list.append(args_complete)
+                index += 1
+
+    # now we have the commands list. print it
+    cpu_count = multiprocessing.cpu_count()
+
+    print('Testing', len(commands_list), 'demos.')
+
+    time_start = time.time()
+
+    Parallel(n_jobs=cpu_count)(delayed(call_eternity)(a) for a in commands_list)
+
+    time_end = time.time()
+    time_elapsed = time_end - time_start
+
+    print()
+
+    print('Elapsed time:', time_elapsed, '; average per demo: ', time_elapsed / len(commands_list))
+
+    temp_file_list = [os.path.join('temp-logs', f) for f in sorted(os.listdir("temp-logs"), key=natural_keys) if
+                      f.endswith(".txt")]
+
+    try:
+        os.remove('demolog.txt')
+    except OSError:
+        pass
+
+    with open('demolog.txt', 'w') as outfile:
+        for f in temp_file_list:
+            with open(f) as infile:
+                outfile.write(infile.read())
+            os.remove(f)
+
+    subprocess.call(['osascript', '-e', 'display notification "Demo testing done!" with title "Eternity demo test"'])
+
+
+if __name__ == "__main__":
+    run_program()
